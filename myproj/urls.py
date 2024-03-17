85f43bfdb47a8a5081623901b8ba485dc2c67803
Usama Nadeem <usamanadeem0786@gmail.com>
2024-03-17 16:28:52 +0500

    static folder updated3

--- myproj/urls.py
+++ myproj/urls.py
@@ -20,5 +20,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("blog.urls",namespace="blog")),
-]
-urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
