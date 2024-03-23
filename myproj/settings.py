b39e6e13545478e7e1fb9b028475d6437cc70c8c
Usama Nadeem <usamanadeem0786@gmail.com>
2024-03-23 14:50:55 +0500

    custom templates and latest feed

--- myproj/settings.py
+++ myproj/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID=1
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'taggit'
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
